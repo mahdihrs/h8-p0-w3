975e5771f17cb6325df3a5b5f3095fb65d54921f
Mahdihrs <mahdiharis@outlook.com>
2018-09-09 14:13:24 +0700

    Latihan mengelompokkan angka

--- [Exercises 14] Mengelompokkan Angka.js
+++ [Exercises 14] Mengelompokkan Angka.js
@@ -27,8 +27,3 @@ console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
 console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
 console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
 console.log(mengelompokkanAngka([])); // [ [], [], [] ]
-
-// Cari tau ada angka berapa aja pake perulangan
-//   Diloop lagi untuk nyari angka genap
-//     Another loop untuk nyari angka ganjil
-//       Another loop untuk nyari angka kelipatan 3
