bf3988f3f65b3cf3f133d71c22e683f53f935c4b
Mahdihrs <mahdiharis@outlook.com>
2018-09-09 14:10:59 +0700

    Logic Challenge - Mengelompokkan Angka

---
+++ [Exercises 14] Mengelompokkan Angka.js
@@ -0,0 +1,34 @@
+function mengelompokkanAngka(arr) {
+  var genap = [];
+  var ganjil = [];
+  var kelipatanTiga = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+      kelipatanTiga.push(arr[i]);
+    } else if (arr[i] % 2 === 0) {
+      genap.push(arr[i]);
+    } else if (arr[i] % 2 !== 0) {
+      ganjil.push(arr[i]);
+    }
+  }
+  var arrFinal = [];
+  arrFinal.push(genap);
+  arrFinal.push(ganjil);
+  arrFinal.push(kelipatanTiga);
+  return arrFinal;
+}
+
+// kelompok pertama (baris pertama) merupakan angka-angka genap
+// kelompok ke-2 (baris ke-2) merupakan angka-angka ganjil
+// kelompok ke-3 (baris ke-3) merupakan angka-angka kelipatan 3
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+
+// Cari tau ada angka berapa aja pake perulangan
+//   Diloop lagi untuk nyari angka genap
+//     Another loop untuk nyari angka ganjil
+//       Another loop untuk nyari angka kelipatan 3
